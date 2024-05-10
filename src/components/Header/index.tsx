6bead32bd7cc3b0e687c2a3908fd68df05395f6e
vanessa <santanavanessa91@gmail.com>
2024-05-10 09:35:57 -0300

    Implementação da estrutura visual da página Complete Order

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,20 +3,25 @@ import { HeaderContainer } from "./styles";
 import coffeeLogoImg from "../../assets/coffee-delivery-logo.svg";
 import { HeaderButton, HeaderButtonsContainer } from "../../layouts/DefaultLayout/styles";
 import { MapPin, ShoppingCart } from 'phosphor-react';
+import { NavLink } from "react-router-dom";
 
 export function Header() {
     return (
        <HeaderContainer>
         <div className="container">
-            <img src={coffeeLogoImg} alt="Logo da Coffee Delivery" />
+            <NavLink to="/">
+                <img src={coffeeLogoImg} alt="Logo da Coffee Delivery" />
+            </NavLink>
         < HeaderButtonsContainer>
             <HeaderButton variant="purple">
                 <MapPin size={20} weight="fill"/>
                 Salvador, BA
             </HeaderButton>
+            <NavLink to="/completeOrder">
             <HeaderButton variant="yellow">
                 <ShoppingCart size={20} weight="fill"/>
-            </HeaderButton>
+                </HeaderButton>
+            </NavLink>
         </ HeaderButtonsContainer>
         
         </div>
